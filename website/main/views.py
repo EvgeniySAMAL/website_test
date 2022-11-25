1bd55899b486461367c24b1e0f8d49e845375c8d
Samal <evgeniy.samal@mail.ru>
2022-11-25 20:57:41 +0300

    дщбавление jinja

--- website/main/views.py
+++ website/main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Task
 
 def index(request):
-    return render(request,'main/index.html')
+    tasks = Task.objects.all()
+    return render(request,'main/index.html', {'title':'Стартовая страница', 'tasks':tasks})
 
 def pictures(request):
     return render(request,'main/pictures.html')
