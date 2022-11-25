5b515b8699d8cb7d87869909e1e59411e5793193
Samal <evgeniy.samal@mail.ru>
2022-11-25 17:11:29 +0300

    дщбавление модели и полей к ней

--- website/main/urls.py
+++ website/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index.html'),
-    path('pictures/',views.pictures),
-    path('about-us/',views.about),
+    path('',views.index, name='home'),
+    path('pictures/',views.pictures, name='pictures'),
+    path('about-us/',views.about, name='about'),
 ]
