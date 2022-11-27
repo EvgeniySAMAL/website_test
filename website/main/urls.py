a01b01ffd3af81a1e5900fe1e64f0f7aebad00d9
Samal <evgeniy.samal@mail.ru>
2022-11-27 15:01:27 +0300

    Сделать альтернативный шаблон для новой страницы.

--- website/main/urls.py
+++ website/main/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='home'),
-    path('pictures/',views.pictures, name='pictures'),
-    path('about-us/',views.about, name='about'),
+    path('pictures',views.pictures, name='pictures'),
+    path('about-us',views.about, name='about'),
+    path('create',views.create, name='create'),
+    path('education',views.education, name='education'),
 ]
